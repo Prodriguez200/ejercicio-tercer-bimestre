775749b720e44a4572ab078fb2147741ca3918b4
Peter <peterrodriguez8865>
2026-05-27 08:28:11 -0600

    FEAT creacion pregunta de Nombre

--- src/Holamundo.java
+++ src/Holamundo.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
 
 public class Holamundo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("oa mondo");
+        System.out.println("Como te llamas");
 
     }
-
+}
 
